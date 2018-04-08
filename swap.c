46a9275430aebaf3052a97478984db637c356d1e
Nikhil Ramakrishnan <ramakrishnan.nikhil@gmail.com>
2018-04-08 10:51:18 +0000

    Added swap.c

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int x = 34;
+    int y = 78;
+
+    printf("x is %d\n", x);
+    printf("y is %d\n", y);
+
+    //swapping code...
+
+    printf("x is now %d\n", x);
+    printf("y is now %d\n", y);
+
+}
